2a0b6b5b3c4118b44680c9b6ed207b6e6e9a8ef9
Jeffrey Gaydos <jeff13gaydos@gmail.com>
2021-12-21 15:58:45 -0500

    versioning revert

--- te-custom-mods.php
+++ te-custom-mods.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Tank Encyclopedia Custom Mods
 Author: Jeffrey Gaydos | Discord: Jeff_G#3210 | Email: jeff13gaydos@gmail.com
-Version: 2.1.0
+Version: 1.0
 
 Description: A plugin created to organize the various functions of the old child theme, thereby removing the need for the child theme. Includes toggles in case any functions fail during udpates.
 */
