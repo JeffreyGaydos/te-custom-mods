dc12ae07e7504e1a04f3f566f2a01e843a3228e9
JeffreyGaydos <jeff13gaydos@gmail.com>
2022-11-30 21:09:45 -0500

    Roshindow's fix for "froen carousel" issues

--- includes/js/tec_random_article.js
+++ includes/js/tec_random_article.js
@@ -2,12 +2,14 @@
  * Creates a random article button located just below the Grow Mediavine social plugin.
  */
 function randomArticleButton() {
-    //random Article Button - needs to be before dark mode that it reacts to the dark mode settings
-    try{
-		document.body.innerHTML += '<a class="tec-rand-link" style="cursor: pointer; width: 40px !important; height: 40px !important; position: fixed; bottom: 50px; left: 20px; background-color: rgba(55, 60, 42, 1); padding: 5px; border-radius: 5px"><img src="https://tanks-encyclopedia.com/wp-content/uploads/2020/05/logo-big-border-30.png" style="padding: 0px 0px 0px 0px;" /></a><div class="tec-rand-text" style="text-align: center; font-size: 12px; position: fixed; left: 12.5px; bottom: 15px;"><p style="font-size:inherit; margin: 0px;" class="wp-caption-text">RANDOM</p><p style="font-size:inherit; margin: 0px;" class="wp-caption-text">ARTICLE</p></div>'
-    } catch (e) {
-        //ignore errors as much as possible
-    }
+  //random Article Button - needs to be before dark mode that it reacts to the dark mode settings
+  try{
+  document.getElementsByClassName("header-titles-wrapper")[0].innerHTML = 
+          document.getElementsByClassName("header-titles-wrapper")[0].innerHTML + 
+          '<a href="https://tanks-encyclopedia.com/?redirect_to=random" class="tec-rand-link" style="cursor: pointer; width: 40px !important; height: 40px !important; position: fixed; bottom: 50px; left: 20px; background-color: rgba(55, 60, 42, 1); padding: 5px; border-radius: 5px"><img src="https://tanks-encyclopedia.com/wp-content/uploads/2020/05/logo-big-border-30.png" style="padding: 0px 0px 0px 0px;" /></a><div class="tec-rand-text" style="text-align: center; font-size: 12px; position: fixed; left: 12.5px; bottom: 15px; width: 55px;"><p style="font-size:inherit; margin: 0px;" class="wp-caption-text">RANDOM</p><p style="font-size:inherit; margin: 0px;" class="wp-caption-text">ARTICLE</p></div>';
+  } catch (e) {
+      //ignore errors as much as possible
+  }
 }
 
 randomArticleButton();
