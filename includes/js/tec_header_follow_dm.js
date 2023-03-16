ce14883169ef513407ce010f9c5cb6f6940b75ca
JeffreyGaydos <jeff13gaydos@gmail.com>
2023-03-15 22:07:36 -0400

    fix header follow dark mode

--- includes/js/tec_header_follow_dm.js
+++ includes/js/tec_header_follow_dm.js
@@ -1,12 +1,13 @@
 //Adds the dark mode button to the clone manually since it is a little slower than the header follow setup
 //stands for Create Dark Mode Button Follow - uses code from tec_dark_mode_init.js
+//deprecated I guess. We no longer append this script to anything
 function cdmbf() {
     let dmDiv = document.createElement("DIV");
     dmDiv.classList.add("tec_dm_div");
     
     let dmButton = document.createElement("BUTTON");
     dmButton.classList.add("tec_dm_button");
-    dmButton.onclick = "tec_darkMode()";
+    dmButton.addEventListener("click", () => tec_darkMode());
 
     let dmP = document.createElement("P");
     dmP.classList.add("tec_dm_p");
