7b1dacb6f1a1a17981a803d32e94ae6a9e6f2afa
JeffreyGaydos <jeff13gaydos@gmail.com>
2023-04-22 17:02:17 -0400

    BUILD: Minimize version updates

--- includes/tec-functions.php
+++ includes/tec-functions.php
@@ -10,7 +10,7 @@ add_action("wp_head", "tec_init", 1);
 
 if( !function_exists("tec_init") ) {
     function tec_init() {
-        wp_enqueue_script( 'tec-init', plugins_url('/js/tec_init.js', __FILE__), '', '1.56');
+        wp_enqueue_script( 'tec-init', plugins_url('/js/tec_init.js', __FILE__), '', '1.0');
     }
 }
 
@@ -24,13 +24,13 @@ if(get_option('tec_dark_mode') == 'on') {
 
 if( !function_exists("tec_dark_mode_create") ) {
     function tec_dark_mode_create() {
-        wp_enqueue_script( 'tec-dark-mode-create', plugins_url('/js/tec_dm_placer.js', __FILE__), '', '2.6');
+        wp_enqueue_script( 'tec-dark-mode-create', plugins_url('/js/tec_dm_placer.js', __FILE__), '', '2.2');
     }
 }
 
 if( !function_exists("tec_dark_mode_init") ) {
     function tec_dark_mode_init() {
-        wp_enqueue_script('tec-dark-mode-init', plugins_url('/js/tec_dm_init.js', __FILE__), '', '3.6');
+        wp_enqueue_script('tec-dark-mode-init', plugins_url('/js/tec_dm_init.js', __FILE__), '', '3.3');
     }
 }
 
@@ -43,7 +43,7 @@ if(get_option('tec_donation') == 'on') {
 
 if( !function_exists("tec_donation_init") ) {
     function tec_donation_init() {
-        wp_enqueue_script( 'tec-donation-init', plugins_url('/js/tec_donation.js', __FILE__), '', '2.5');
+        wp_enqueue_script( 'tec-donation-init', plugins_url('/js/tec_donation.js', __FILE__), '', '2.4');
     }
 }
 
@@ -56,7 +56,7 @@ if(get_option('tec_header_follow') == 'on') {
 
 if( !function_exists("tec_header_follow_init") ) {
     function tec_header_follow_init() {
-        wp_enqueue_script( 'tec-header-follow-init', plugins_url('/js/tec_header_follow.js', __FILE__), '', '2.8');
+        wp_enqueue_script( 'tec-header-follow-init', plugins_url('/js/tec_header_follow.js', __FILE__), '', '2.1');
     }
 }
 
@@ -69,7 +69,7 @@ if(get_option('tec_random_article') == 'on') {
 
 if( !function_exists("tec_random_article_init") ) {
     function tec_random_article_init() {
-        wp_enqueue_script( 'tec-random-article-init', plugins_url('/js/tec_random_article.js', __FILE__), '', '2.12');
+        wp_enqueue_script( 'tec-random-article-init', plugins_url('/js/tec_random_article.js', __FILE__), '', '2.5');
     }
 }
 
@@ -95,7 +95,7 @@ if(get_option('tec_index') == 'on') {
 
 if(!function_exists("tec_index_generate")) {
     function tec_index_generate() {
-        wp_enqueue_script('tec-index-gen', plugins_url('/js/tec_index.js', __FILE__), '', '2.3');
+        wp_enqueue_script('tec-index-gen', plugins_url('/js/tec_index.js', __FILE__), '', '2.1');
     }
 }
 /********************************************************************
@@ -122,7 +122,7 @@ if(get_option('tec_mobile') == 'on') {
 
 if( !function_exists("tec_mobile_init") ) {
     function tec_mobile_init() {
-        wp_enqueue_script( 'tec-mobile-init', plugins_url('/js/tec_mobile_comp_init.js', __FILE__), '', '2.4');
+        wp_enqueue_script( 'tec-mobile-init', plugins_url('/js/tec_mobile_comp_init.js', __FILE__), '', '2.2');
     }
 }
 
@@ -135,7 +135,7 @@ if(get_option('tec_to_top') == 'on') {
 
 if( !function_exists("tec_to_top") ) {
     function tec_to_top() {
-        wp_enqueue_script( 'tec-to-top', plugins_url('/js/tec_to_top.js', __FILE__), '', '2.12');
+        wp_enqueue_script( 'tec-to-top', plugins_url('/js/tec_to_top.js', __FILE__), '', '2.4');
     }
 }
 
@@ -143,13 +143,13 @@ if( !function_exists("tec_to_top") ) {
  * Routing Recent Video Embed
  ********************************************************************/
 if(get_option('tec_video_embed') == 'on') {
-    add_action("wp_head", "tec_video_embed", 10); // we probably want this to go last...
+    add_action("wp_head", "tec_video_embed", 10);
 }
 
 if( !function_exists("tec_video_embed") ) {
     function tec_video_embed() {
         if(is_single() && !in_category("has-own-video")) {
-            wp_enqueue_script( 'tec-video-embed', plugins_url('/js/tec_video_embed.js', __FILE__), '', '2.88');
+            wp_enqueue_script( 'tec-video-embed', plugins_url('/js/tec_video_embed.js', __FILE__), '', '1.0');
         }
     }
 }
