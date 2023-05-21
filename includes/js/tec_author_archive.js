0384495fe030bb059dbfdfbdba1f290e375c0564
JeffreyGaydos <jeff13gaydos@gmail.com>
2023-05-21 16:31:17 -0400

    hack to override author archive page from plugin

---
+++ includes/js/tec_author_archive.js
@@ -0,0 +1,13 @@
+function tec_activate_author_archive_php() {
+    var newPage = document.querySelector("main#tec_author_archive");
+    var oldPage = document.querySelector("main#site-content");
+
+    console.log(newPage);
+    console.log(oldPage);
+
+    oldPage.innerHTML = newPage.innerHTML;
+    oldPage.style.display = "block";
+    newPage.style.display = "none";
+}
+
+tec_activate_author_archive_php();
--- includes/tec-functions.php
+++ includes/tec-functions.php
@@ -173,3 +173,71 @@ if( !function_exists("tec_coauthor_display")) {
         }
     }
 }
+
+add_action("wp_head", "tec_author_archive", 12);
+
+function tec_author_archive() {
+    if(is_author()) {
+?>
+<main style="display:block" id="tec_author_archive" role="main">
+
+	<?php
+
+	$archive_title    = '';
+	$archive_subtitle = '';
+
+	if ( ! is_home() ) {
+		$archive_title    = get_the_archive_title();
+		$archive_subtitle = get_the_archive_description();
+	}
+
+	if ( $archive_title || $archive_subtitle ) {
+		?>
+
+		<header class="archive-header has-text-align-center header-footer-group">
+
+			<div class="archive-header-inner section-inner medium">
+
+				<?php if ( $archive_title ) { ?>
+					<h1 class="archive-title"><?php echo wp_kses_post( $archive_title ); ?></h1>
+				<?php } ?>
+
+				<?php if ( $archive_subtitle ) { ?>
+					<div class="archive-subtitle section-inner thin max-percentage intro-text"><?php echo wp_kses_post( wpautop( $archive_subtitle ) ); ?></div>
+				<?php } ?>
+
+			</div><!-- .archive-header-inner -->
+
+		</header><!-- .archive-header -->
+
+		<?php
+	}
+	
+	//excerpt 'continue reading' link------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	function twentytwentychild_excerpt_more_add_continue_reading( $more ) {
+		return ' ... <div class="read-more-button-wrap"><a href="' . get_permalink( get_the_ID() ) . '" class="more-link"><span class="faux-button">Continue reading</span> <span class="screen-reader-text">“' . get_the_title( get_the_ID() ) . '”</span></a></div>';
+	}
+	add_filter('excerpt_more', 'twentytwentychild_excerpt_more_add_continue_reading' );
+	//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	if ( have_posts() ) : 
+		$i = 0;
+		while ( have_posts() ) : the_post(); 
+			$i++;
+			if ( $i > 1 ) {
+				echo '<hr class="post-separator styled-separator is-style-wide section-inner" aria-hidden="true" />';
+			}
+			the_title( '<h2>', '</h2>' ); 
+			the_excerpt();
+		endwhile; 
+	else: 
+		_e( 'Sorry, no posts matched your criteria.', 'textdomain' ); 
+	endif; 
+	?>
+
+</main><!-- #site-content -->
+
+<?php
+        wp_enqueue_script( 'tec-author-archive', plugins_url('/js/tec_author_archive.js', __FILE__), '', '1.7');
+    }
+}
