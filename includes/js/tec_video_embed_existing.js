8f7bdf2e9250834518778057f55378c19c7fb539
JeffreyGaydos <jeff13gaydos@gmail.com>
2023-06-12 12:51:12 -0400

    initial structure for new scripts

---
+++ includes/js/tec_video_embed_existing.js
@@ -0,0 +1,6 @@
+tec_reformat_existing_embed_video();
+
+function tec_reformat_existing_embed_video() {
+    //make sure the x is there
+    //add maximization feature
+}
--- includes/tec-functions.php
+++ includes/tec-functions.php
@@ -148,8 +148,12 @@ if(get_option('tec_video_embed') == 'on') {
 
 if( !function_exists("tec_video_embed") ) {
     function tec_video_embed() {
-        if(is_single() && !in_category("has-own-video")) {
-            wp_enqueue_script( 'tec-video-embed', plugins_url('/js/tec_video_embed.js', __FILE__), '', '1.2');
+        if(is_single()) {
+            if(is_single() && !in_category("has-own-video")) {
+                wp_enqueue_script( 'tec-video-embed', plugins_url('/js/tec_video_embed.js', __FILE__), '', '1.2');
+            } else {
+                wp_enqueue_script( 'tec-video-embed-existing', plugins_url('/js/tec_video_embed_existing.js', __FILE__), '', '1.0');
+            }
         }
     }
 }
