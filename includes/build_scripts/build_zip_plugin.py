ceaee0bd166b4a1b442ac1065768b2b81c2a0722
JeffreyGaydos <jeff13gaydos@gmail.com>
2023-07-30 15:31:32 -0400

    re-build zipper (and better) with the help of AI and regex

--- includes/build_scripts/build_zip_plugin.py
+++ includes/build_scripts/build_zip_plugin.py
@@ -1,55 +1,47 @@
-printPrefix = "[build_zip_plugin.py]: "
-
-import shutil
+import zipfile
 import os
 import datetime
+import re
+
+###########################################################################################
+# Global / Utility
+###########################################################################################
+
+printPrefix = "[build_zip_plugin.py]: "
+
+warn = "[WARNING] " #"⚠️  "
+err = "[ERROR] " #"⛔ "
+info = "[INFO] " #"📎 "
 
 def pprint(any):
     print(f"{datetime.datetime.now()} {printPrefix}{any}")
 
-buildDir = "build"
 
-if(os.path.exists(buildDir)):
-    shutil.rmtree(buildDir)
-# shutil.copy("../")
-os.mkdir(buildDir) # put everything that needs zipped in here
-pprint("created " + buildDir)
-os.mkdir(buildDir + "/includes")
-pprint("created " + buildDir + "/includes")
+def stop(success):
+    if(success):
+        #pprint("✔ Completed successfully")
+        pprint("Completed successfully")
+    else:
+        #pprint("✘ Ended prematurely")
+        pprint("Ended prematurely")
+        exit()
 
-jsPrefix = "./includes/js"
-jsDest = buildDir + "/includes/js"
-os.mkdir(jsDest)
-pprint("created " + jsDest)
-for jsfile in os.listdir(jsPrefix):
-    shutil.copy(jsPrefix + "/" + jsfile, jsDest)
-    pprint("copied '" + jsfile + "' to '" + jsDest + "'")
-cssPrefix = "./includes/css"
-cssDest = buildDir + "/includes/css"
-os.mkdir(cssDest)
-pprint("created " + cssDest)
-for cssfile in os.listdir(cssPrefix):
-    shutil.copy(cssPrefix + "/" + cssfile, cssDest)
-    pprint("copied '" + cssfile + "' to '" + cssDest + "'")
-shutil.copy("./includes/tec-functions.php", buildDir + "/includes")
-pprint("copied 'tec-functions.php'")
-shutil.copy("./te-custom-mods.php", buildDir)
-pprint("copied 'te-custom-mods.php'")
-imgPrefix = "./images"
-imgDest = buildDir + "/images"
-os.mkdir(imgDest)
-pprint("created " + imgDest)
-for img in os.listdir(imgPrefix):
-    shutil.copy(imgPrefix + "/" + img, imgDest)
-    pprint("copied '" + img + "' to '" + imgDest + "'")
+###########################################################################################
+# zip files
+###########################################################################################
 
-pprint("zipping...")
-zipName = "te-custom-mods"
-zipExt = "zip"
-shutil.make_archive(zipName, zipExt, buildDir)
-pprint("zipped to '" + zipName + zipExt + "'")
+regInclude = ["(?<!\.orig)\.js$", "(?<!\.orig)\.css$", "(?<!\.orig)\.png$", "(?<!\.orig)\.php$" ]
+fileExclude = ["additional.css"]
 
-if(os.path.exists(buildDir)):
-    shutil.rmtree(buildDir)
-pprint("Deleted build directory. Build Complete")
+def zip_files(directory):
+    with zipfile.ZipFile('te-custom-mods.zip', 'w') as zipf:
+        for root, files in os.walk(directory):
+            for file in files:
+                if fileExclude.__contains__(file):
+                    continue
+                for reg in regInclude:
+                    if re.search(reg, file) is not None:
+                        pprint(file)
+                        zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), directory))
 
+zip_files('./')
