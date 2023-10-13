e82621b0a635f2ab40b92961737fc8a7e45262d0
JeffreyGaydos <jeff13gaydos@gmail.com>
2023-10-12 23:47:48 -0400

    upload RSS update scripts

---
+++ RSS/update_rss.py
@@ -0,0 +1,32 @@
+import sys
+import datetime
+
+_date = "{{date}}"
+_url = "{{url}}"
+_title ="{{title}}"
+
+print("Updating RSS feeds...")
+
+base = open("./RSS/recent-articles.base.rss").read()
+
+dateString = (str)(datetime.datetime.now())
+print(dateString[:10])
+
+regexOutput = sys.argv[1]
+
+urlString = regexOutput.split("\">")[0]
+titleString = regexOutput.split("\">")[1]
+print(urlString)
+print(titleString)
+
+# base = base.replace(_date, dateString[:10])
+base = base.replace(_date, dateString)
+base = base.replace(_url, urlString)
+base = base.replace(_title, titleString)
+
+print(base)
+
+rss = open("./RSS/recent-articles.rss", "w")
+rss.write(base)
+
+print("Finished updating RSS feeds...")
