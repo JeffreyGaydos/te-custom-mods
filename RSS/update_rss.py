bfdf439989b829b77bdfaf0ed2f132bd832be71f
JeffreyGaydos <jeff13gaydos@gmail.com>
2023-10-14 22:11:51 -0400

    try something new using requests package

--- RSS/update_rss.py
+++ RSS/update_rss.py
@@ -1,5 +1,7 @@
 import sys
 import datetime
+import requests #python -m pip install requests
+import re
 
 _date = "{{date}}"
 _url = "{{url}}"
@@ -7,18 +9,27 @@ _title ="{{title}}"
 
 print("Updating RSS feeds...")
 
-base = open("./RSS/recent-articles.base.rss").read()
+#get data
+requestsResponse = requests.get("https://tanks-encyclopedia.com/", headers={'user-agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'})
+rawContent = requestsResponse.content
+recentPostsRGX = "(?<=<nav aria-label=\"Recent Posts\">).+(?=</nav>)"
+recentPostsHTML = re.search(recentPostsRGX, (str)(rawContent)).group(0)
+mostRecentPost = recentPostsHTML.split("</a>")[0]
+mostRecentPostTitle = mostRecentPost.split('">')[1]
+mostRecentPostLink = mostRecentPost.split('">')[0].split('<a href="')[1]
+print("Article Title: "  + mostRecentPostTitle)
+print("Article Link: " + mostRecentPostLink)
 
 dateString = (str)(datetime.datetime.now())
-print(dateString[:10])
+print("RSS Update Date: " + dateString[:10])
 
-regexOutput = sys.stdin.read()
-print("Input from Regex: " + regexOutput)
-
-urlString = regexOutput.split('">')[0]
+#update RSS file
+urlString = mostRecentPostLink
 print(urlString)
-titleString = regexOutput.split('">')[1]
+titleString = mostRecentPostTitle
 print(titleString)
+
+base = open("./RSS/recent-articles.base.rss").read()
 
 # base = base.replace(_date, dateString[:10])
 base = base.replace(_date, dateString)
