2770b5e8bc8423509bc722a464a31114ce272cc1
JeffreyGaydos <jeff13gaydos@gmail.com>
2023-11-05 00:27:47 -0400

    fix index render bug by making CSS render-blocking

--- includes/js/tec_init.js
+++ includes/js/tec_init.js
@@ -1,7 +1,8 @@
 var tec_default_style = document.createElement("link");
 tec_default_style.rel = "stylesheet";
-tec_default_style.href = "/wp-content/plugins/te-custom-mods/includes/css/tec_default.css?v1.117";
+tec_default_style.href = "/wp-content/plugins/te-custom-mods/includes/css/tec_default.css?v1.118";
 tec_default_style.id = "tec_default_stylesheet";
+tec_default_style.blocking = "render";
 document.head.appendChild(tec_default_style);
 
 //Utility functions
