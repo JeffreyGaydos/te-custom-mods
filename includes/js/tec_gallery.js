a3f91b4ee991bcc33b9fccb01f87db2f3a84cf69
JeffreyGaydos <55904615+JeffreyGaydos@users.noreply.github.com>
2024-02-17 10:07:00 -0500

    Gallery: Refactor with Better JS/CSS Knowledge (#72)
    * implement captions with static height container
    
    * remove extra elements that we don't specifically check for in galleries
    
    * simplify (and fix) the way we get gallery images & captions
    
    * fix index state change issues
    
    * finalize fade and height maintainence mechanics
    
    * make gallery arrows mobile friendly
    
    * correct dark mode coloring for gallery; remove highlight on next/previous
    
    * add autoplay feature
    
    * remove unused code
    
    * woops move default styles for autoplay feature to default stylesheet
    
    * remove unused function
    
    * GHA_BUILD: Minimize JS Versions
    
    * GHA_BUILD: Minimize CSS Versions
    
    * GHA_BUILD: Increment Plugin Version to 2.1.18
    
    * GHA_BUILD: Zip Plugin
    
    ---------
    
    Co-authored-by: GitHub Actions <actions@github.com>

--- includes/js/tec_gallery.js
+++ includes/js/tec_gallery.js
@@ -1,10 +1,10 @@
 //Global variables for the gallery mechanics, below
-var ImageArray = new Array();
-var AllImagesArray = new Array();
-var CaptionArray = new Array();
-var AllCaptionsArray = new Array();
-var paddingArray = new Array();
-var currentIndexes = new Array();
+var tec_ImageArray = new Array();
+var tec_AllImagesArray = new Array();
+var tec_CaptionArray = new Array();
+var tec_AllCaptionsArray = new Array();
+var tec_currentIndexes = new Array();
+var tec_intervalArray = new Array();
 
 /*
 	The main function for creating galleries. Call this and all images surrounded by div tags
@@ -12,187 +12,185 @@ var currentIndexes = new Array();
 	are supported. Uses javascript and JQuery.
 */
 function createGalleries() {
-	var g = 0;
-	while(g < document.getElementsByClassName("tec-gallery").length) {
-		//must be first so that "maintainWidth" can find the proper index
-		currentIndexes.push(0);
-		paddingArray.push(new Array());
+	const numGalleries = document.getElementsByClassName("tec-gallery").length;
+	for(var g = 0; g < numGalleries; g++) {
+		tec_currentIndexes.push(0);
 		createGallery(g);
-		g++;
 	}
 }
 
 createGalleries();
 
+function tec_setFeaturedImage(g_num, index) {
+	tec_safe_remove_class(document.querySelector(`.tec-gallery:nth-of-type(${g_num + 1}) img.tec_g_image.tec_g_featured`), "tec_g_featured");
+
+	var desiredImage = document.querySelector(`.tec-gallery:nth-of-type(${g_num + 1}) img.tec_g_image:nth-of-type(${index + 1})`);
+	desiredImage.classList.add("tec_g_featured");
+
+	tec_safe_remove_class(document.querySelector(`.tec-gallery:nth-of-type(${g_num + 1}) div.tec_g_caption_wrapper p.tec_g_caption.tec_g_featured`), "tec_g_featured");
+
+	var desiredCaption = document.querySelector(`.tec-gallery:nth-of-type(${g_num + 1}) div.tec_g_caption_wrapper p.tec_g_caption:nth-of-type(${index + 1})`);
+	desiredCaption.classList.add("tec_g_featured");
+
+	var fauxCaption = document.querySelector(`.tec-gallery:nth-of-type(${g_num + 1}) div.tec_g_caption_wrapper p.tec_g_faux_caption`);
+	fauxCaption.innerText = tec_AllCaptionsArray[g_num][index];
+
+	tec_safe_remove_class(document.querySelector(`.tec-gallery:nth-of-type(${g_num + 1}) div.tec_g_indicator.tec_g_featured`), "tec_g_featured");
+
+	var desiredIndicator = document.querySelector(`.tec-gallery:nth-of-type(${g_num + 1}) div.tec_g_indicator:nth-of-type(${index + 1})`);
+	desiredIndicator.classList.add("tec_g_featured");
+
+	tec_currentIndexes[g_num] = index;
+}
+
 /*
 	Creates 1 gallery by storing the image sources and captions into global arrays and
 	adding the structure of the gallery.
 */
-function createGallery(g_num, fig = false) {
-	var i = 0;
-	while(document.getElementsByClassName("tec-gallery")[g_num].getElementsByTagName("img").length > 0) {
-		//console.log("Found Image: " + document.getElementsByClassName("tec-gallery")[g_num].getElementsByTagName("img")[0].src);
-		var img = document.getElementsByClassName("tec-gallery")[g_num].getElementsByTagName("img")[0];
-		ImageArray.push(img.src);
-		document.getElementsByClassName("tec-gallery")[g_num].getElementsByTagName("img")[0].remove();
-	}
-	AllImagesArray.push(ImageArray);
-	if(document.getElementsByClassName("tec-gallery")[g_num].getAttribute("figure") == '') {
-		while(document.getElementsByClassName("tec-gallery")[g_num].getElementsByTagName("figcaption").length > 0) {
-			//console.log("Found Caption: " + document.getElementsByClassName("tec-gallery")[g_num].getElementsByTagName("em")[0].innerHTML);
-			CaptionArray.push(document.getElementsByClassName("tec-gallery")[g_num].getElementsByTagName("figcaption")[0].innerHTML);
-			document.getElementsByClassName("tec-gallery")[g_num].getElementsByTagName("figcaption")[0].remove();
-		}
-	} else {
-		while(document.getElementsByClassName("tec-gallery")[g_num].getElementsByTagName("em").length > 0) {
-			//console.log("Found Caption: " + document.getElementsByClassName("tec-gallery")[g_num].getElementsByTagName("em")[0].innerHTML);
-			CaptionArray.push(document.getElementsByClassName("tec-gallery")[g_num].getElementsByTagName("em")[0].innerHTML);
-			document.getElementsByClassName("tec-gallery")[g_num].getElementsByTagName("em")[0].remove();
-		}
-	}
-	AllCaptionsArray.push(CaptionArray);
-	var galleryBody = "";
+function createGallery(g_num) {
+	document.querySelectorAll(`.tec-gallery:nth-of-type(${g_num + 1}) img`).forEach(ie => {
+		tec_ImageArray.push(ie.src);
+	});
+	tec_AllImagesArray.push(tec_ImageArray);
+	document.querySelectorAll(`.tec-gallery:nth-of-type(${g_num + 1}) figcaption, .tec-gallery:nth-of-type(${g_num + 1}) em`).forEach(fc => {
+		tec_CaptionArray.push(fc.innerHTML);
+	});
+
+	var galleryChildren = [];
+	document.querySelector(`.tec-gallery:nth-of-type(${g_num + 1})`).childNodes.forEach(c => {
+		galleryChildren.push(c);
+	});
+	galleryChildren.forEach(c => {
+		document.querySelector(`.tec-gallery:nth-of-type(${g_num + 1})`).removeChild(c);
+	});
+
+	tec_AllCaptionsArray.push(tec_CaptionArray);
+	var gallery = document.createElement("DIV");
+	gallery.classList.add("tec_g_body");
+	gallery.setAttribute("name", "g_border");
+	var galleryInnerWrapper = document.createElement("DIV");
+	galleryInnerWrapper.classList.add("tec_g_inner_wrapper");
 	
-	//border and featured image
-	galleryBody += '<div style="width: 100%; border: 1px solid black; display: inline-block;" class="tec_g_border" name="g_border"><div style="display: flex; justify-content: center;"><img src="';
-	galleryBody += ImageArray[0];
-	if(document.getElementsByClassName("tec-gallery")[g_num].getAttribute("tec-g-height") != null) {
-		galleryBody += '" style="height: ' + document.getElementsByClassName("tec-gallery")[g_num].getAttribute("tec-g-height") + 'px; padding: 50px 0px; display: inline-block;" name="tec_g_featured"></div>';
-	} else {
-		galleryBody += '" style="height: 300px; padding: 50px 0px 25px 0px; display: inline-block;" name="tec_g_featured"></div>';
-	}
-
-	//position indicators
-	if(ImageArray[g_num].length * 35.5 < screen.width) {
-		galleryBody += '<div class="tec_g_pos_ind_desktop" style="display: flex; width: 100%; justify-content: center; padding-bottom: 5px;">';
-		galleryBody += '<div onclick="tec_g_jump(' + g_num + ', 1, ' + i + ');" class="tec_g_pos_ind current" onclick="" onmouseenter="this.classList.add(\'hovered\')" onmouseleave="this.classList.remove(\'hovered\')" style="cursor: pointer; flex-grow: 1; height: 15px; border: 2px solid black; margin: 5px; max-width: 50px; background-color: rgb(255, 255, 255);"></div>';
-		for(var i = 1; i < ImageArray.length; i++) {
-			galleryBody += '<div onclick="tec_g_jump(' + g_num + ', 1, ' + i + ');" class="tec_g_pos_ind" onclick="" onmouseenter="this.classList.add(\'hovered\')" onmouseleave="this.classList.remove(\'hovered\')" style="cursor: pointer; flex-grow: 1; height: 15px; border: 2px solid black; margin: 5px; max-width: 50px; background-color: rgb(255, 255, 255);"></div>';
-		}
-		galleryBody += '</div>';
-	} else {
-		galleryBody += '<div class="tec_g_pos_ind_mobile" style="display: flex; width: 100%; justify-content: center; padding-bottom: 15px">';
-		galleryBody += '<h4 style="margin: inherit;">1/' + ImageArray.length + '</h4>';
-		galleryBody += '</div>';
-	}
-
-	galleryBody += '</div>';
-	galleryBody += '<div style="margin-top: 10px; display: flex; justify-content: center; width: 100%;">';
-
-	//arrow left
-	galleryBody += '<div class="tec_g_arrow" style="flex-grow: 1; display: flex; justify-content: center; border: 2px solid black; padding: 4px; height: 30px; cursor: pointer;" onMouseOver="this.style.backgroundColor=\'#f6dd95\'" onMouseOut="this.style.backgroundColor=\'white\'" onclick="tec_g_previous(';
-	galleryBody += g_num + ", 1";
-	galleryBody += ')" class="tec_g_arrow" name="g_previous">';
-	galleryBody += '<img class="tec_g_arrow_img" src="/wp-content/plugins/te-custom-mods/images/DropdownBlack.png" style="transform: rotate(90deg); width: 18px; height: 18px;">';
-	galleryBody += "</div>";
-
-	//caption
-	galleryBody += '<p style="width: calc(90% - 4rem - 22px); text-align: center; border: 0px solid black; padding: 0px 0px; margin: 0px; margin-right: 0px; font-style: italic;" class="entry-content tec_g_caption" name="g_caption">';
-	if(CaptionArray[0] == undefined) {
-		galleryBody += 'No caption available.';
-	} else {
-		galleryBody += CaptionArray[0];
-	}
-    galleryBody += '</p>';
-	//arrow right
-	galleryBody += '<div class="tec_g_arrow" style="flex-grow: 1; display: flex; justify-content: center; border: 2px solid black; padding: 4px; height: 30px; cursor: pointer;" onMouseOver="this.style.backgroundColor=\'#f6dd95\'" onMouseOut="this.style.backgroundColor=\'white\'" style="height: 22px; width: 62px; padding-right: 20px; padding-left: 20px; margin-top: 141px; margin-bottom: 141px; transform: rotate(-90deg); border: 2px solid black; cursor: pointer" onclick="tec_g_next(';
-	galleryBody += g_num + ", 1";
-	galleryBody += ')" class="tec_g_arrow" name="g_next">';
-	galleryBody += '<img class="tec_g_arrow_img" src="/wp-content/plugins/te-custom-mods/images/DropdownBlack.png" style="transform: rotate(-90deg); width: 18px; height: 18px;">';
-	galleryBody += "</div>";
-
-	galleryBody += '</div>';
-
-	document.body.classList.remove("hovered")
-
-	document.getElementsByClassName("tec-gallery")[g_num].innerHTML = galleryBody;
-	ImageArray = new Array();
-	CaptionArray = new Array();
-}
-
-/*
-	Moves whichever gallery'slide to the previous image and caption.
-	Called by the arrow buttons themselves.
-*/
-function tec_g_previous(g_num, fade) {
-	//console.log("Backwards");
-	$('p[name="g_caption"]:eq(' + g_num + ')').fadeOut(500 * fade).fadeIn(500 * fade);
-	$('img[name="tec_g_featured"]:eq(' + g_num + ')').fadeTo(500 * fade, 0.00001, function() {
-		//console.log("Fired Callback. g_num: " + g_num);
-		currentIndexes[g_num]--;
-		if(currentIndexes[g_num] < 0) {
-			currentIndexes[g_num] = AllImagesArray[g_num].length - 1;
-		}
-		tec_g_udpate_pos_ind(g_num, currentIndexes[g_num]);
-		document.getElementsByName("tec_g_featured")[g_num].setAttribute("src", AllImagesArray[g_num][currentIndexes[g_num]]);
-		if(AllCaptionsArray[g_num][currentIndexes[g_num]]) {
-			document.getElementsByName("g_caption")[g_num].innerHTML = AllCaptionsArray[g_num][currentIndexes[g_num]];
+	var galleryPlayPauseWrapper = document.createElement("DIV");
+	galleryPlayPauseWrapper.classList.add("tec_g_play_pause_wrapper");
+	var galleryPlayPause = document.createElement("DIV");
+	galleryPlayPause.classList.add("tec_g_play_pause");
+	galleryPlayPause.innerText = "⏯︎";
+	galleryPlayPause.title = "auto-play this gallery";
+	const gNumConst = g_num;
+	galleryPlayPause.addEventListener("click", (e) => {
+		window.getSelection().removeAllRanges();
+		if(!tec_intervalArray[g_num]) {
+			e.target.classList.add("pressed");
+			document.querySelector(`.tec-gallery:nth-of-type(${gNumConst + 1}) .tec_g_arrow.right`).click();
+			tec_intervalArray[g_num] = setInterval(() => {
+				document.querySelector(`.tec-gallery:nth-of-type(${gNumConst + 1}) .tec_g_arrow.right`).click();
+			}, 4000);
 		} else {
-			document.getElementsByName("g_caption")[g_num].innerHTML = "No caption available.";
+			tec_safe_remove_class(e.target, "pressed");
+			clearInterval(tec_intervalArray[g_num]);
+			tec_intervalArray[g_num] = undefined;
 		}
-		document.getElementsByName("tec_g_featured")[g_num].style.padding = "50px 0px";
-	}).fadeTo(500 * fade, 1);
-}
+	});
+	galleryPlayPauseWrapper.appendChild(galleryPlayPause);
+	galleryInnerWrapper.appendChild(galleryPlayPauseWrapper);
 
-/*
-	Moves whichever gallery'slide to the next image and caption.
-	Called by the arrow buttons themselves.
-*/
-function tec_g_next(g_num, fade) {
-	//console.log("Forwards");
-	$('p[name="g_caption"]:eq(' + g_num + ')').fadeOut(500 * fade).fadeIn(500 * fade);
-	$('img[name="tec_g_featured"]:eq(' + g_num + ')').fadeTo(500 * fade, 0.00001, function() {
-    	//console.log("Fired Callback. g_num: " + g_num);
-		currentIndexes[g_num]++;
-		if(currentIndexes[g_num] > AllImagesArray[g_num].length - 1) {
-	    currentIndexes[g_num] = 0;
-    	}
-		tec_g_udpate_pos_ind(g_num, currentIndexes[g_num]);
-		document.getElementsByName("tec_g_featured")[g_num].setAttribute("src", AllImagesArray[g_num][currentIndexes[g_num]]);
-		if(AllCaptionsArray[g_num][currentIndexes[g_num]]) {
-			document.getElementsByName("g_caption")[g_num].innerHTML = AllCaptionsArray[g_num][currentIndexes[g_num]];
-		} else {
-			document.getElementsByName("g_caption")[g_num].innerHTML = "No caption available.";
-		}
-		document.getElementsByName("tec_g_featured")[g_num].style.padding = "50px 0px";
-	}).fadeTo(500 * fade, 1);
-}
-
-/*
-	Moves the slideshow to a particular position using the position
-	indicators.
-*/
-function tec_g_jump(g_num, fade, index) {
-	if(currentIndexes[g_num] == index)
-		return; //don't want to "jump" to the current image
-	$('p[name="g_caption"]:eq(' + g_num + ')').fadeOut(500 * fade).fadeIn(500 * fade);
-	$('img[name="tec_g_featured"]:eq(' + g_num + ')').fadeTo(500 * fade, 0.00001, function() {
-		currentIndexes[g_num] = index;
+	var indicatorContainer = document.createElement("DIV");
+	indicatorContainer.classList.add("tec_g_indicator_container");
+	var galleryImages = [];
+	var galleryFauxImages = [];
+	var positionIndicators = [];
+	for(var i = 0; i < tec_ImageArray.length; i++) {
+		const j = i; //to make i not be a reference var
+		var image = document.createElement("IMG");
+		image.src = tec_ImageArray[i];
+		image.classList.add("tec_g_image");
+		galleryImages.push(image);
 		
-		document.getElementsByName("tec_g_featured")[g_num].setAttribute("src", AllImagesArray[g_num][currentIndexes[g_num]]);
-		if(AllCaptionsArray[g_num][currentIndexes[g_num]]) {
-			document.getElementsByName("g_caption")[g_num].innerHTML = AllCaptionsArray[g_num][currentIndexes[g_num]];
-		} else {
-			document.getElementsByName("g_caption")[g_num].innerHTML = "No caption available.";
-		}
-		document.getElementsByName("tec_g_featured")[g_num].style.padding = "50px 0px";
-	}).fadeTo(500 * fade, 1);
-	tec_g_udpate_pos_ind(g_num, index);
-}
+		//to maintain height when featured is the largest image
+		var fauxImage = document.createElement("IMG");
+		fauxImage.src = tec_ImageArray[i];
+		fauxImage.classList.add("tec_g_faux_image");
+		fauxImage.setAttribute("loading", "lazy");
+		galleryFauxImages.push(fauxImage);
 
-/*
-	Updates the styling via a class so that the position
-	indicators actually indicate which position in the slide
-	show you are at. Styling is done externally in CSS.
-*/
-function tec_g_udpate_pos_ind(g_num, index) {
-	var gallery = document.getElementsByClassName("tec-gallery")[g_num];
-	if(gallery.getElementsByClassName("tec_g_pos_ind").length > 0) {
-		for(var i = 0; i < gallery.getElementsByClassName("tec_g_pos_ind").length; i++) {
-			gallery.getElementsByClassName("tec_g_pos_ind")[i].classList.remove("current");	
+		var indicator = document.createElement("DIV");
+		indicator.addEventListener("click", () => tec_setFeaturedImage(g_num, j));
+		indicator.classList.add("tec_g_indicator");
+		if(i == 0) {
+			indicator.classList.add("tec_g_featured");
 		}
-		gallery.getElementsByClassName("tec_g_pos_ind")[index].classList.add("current");
-	} else {
-		gallery.getElementsByClassName("tec_g_pos_ind_mobile")[0].getElementsByTagName("h4")[0].innerHTML = (index + 1) + '/' + AllImagesArray[g_num].length;
+		positionIndicators.push(indicator);
 	}
+	galleryImages[0].classList.add("tec_g_featured");
+
+	var galleryFooter = document.createElement("DIV");
+	galleryFooter.classList.add("tec_g_footer");
+	
+	var leftArrow = document.createElement("DIV");
+	leftArrow.classList.add("tec_g_arrow");
+	leftArrow.classList.add("left");
+	leftArrow.addEventListener("click", () => {
+		window.getSelection().removeAllRanges();
+		tec_setFeaturedImage(g_num, tec_currentIndexes[g_num] == 0 ? maxIndex - 1 : (tec_currentIndexes[g_num] - 1) % maxIndex)
+	});
+	var leftArrowImageLight = document.createElement("IMG");
+	leftArrowImageLight.src = "/wp-content/plugins/te-custom-mods/images/DropdownBlack.png";
+	var leftArrowImageDark = document.createElement("IMG");
+	leftArrowImageDark.src = "/wp-content/plugins/te-custom-mods/images/DropdownYellow.png";
+	leftArrowImageDark.classList.add("dark");
+	leftArrow.appendChild(leftArrowImageLight);
+	leftArrow.appendChild(leftArrowImageDark);
+	galleryFooter.appendChild(leftArrow);
+
+	var captions = document.createElement("DIV");
+	captions.classList.add("tec_g_caption_wrapper");
+	for(var i = 0; i < tec_CaptionArray.length; i++) {
+		var captionP = document.createElement("P");
+		captionP.innerText = tec_CaptionArray[i] ?? "No caption available.";
+		captionP.classList.add("tec_g_caption");
+		if(i == 0) {
+			captionP.classList.add("tec_g_featured");
+		}
+		captions.appendChild(captionP);
+	}
+	var fauxCaption = document.createElement("P");
+	fauxCaption.innerText = tec_CaptionArray[0];
+	fauxCaption.classList.add("tec_g_faux_caption");
+	fauxCaption.classList.add("tec_g_featured");
+	captions.appendChild(fauxCaption);
+
+	galleryFooter.appendChild(captions);
+
+	const maxIndex = tec_ImageArray.length;
+
+	var rightArrow = document.createElement("DIV");
+	rightArrow.classList.add("tec_g_arrow");
+	rightArrow.classList.add("right");
+	rightArrow.addEventListener("click", () => {
+		window.getSelection().removeAllRanges();
+		tec_setFeaturedImage(g_num, (tec_currentIndexes[g_num] + 1) % maxIndex)
+	});
+	var rightArrowImageLight = document.createElement("IMG");
+	rightArrowImageLight.src = "/wp-content/plugins/te-custom-mods/images/DropdownBlack.png";
+	var rightArrowImageDark = document.createElement("IMG");
+	rightArrowImageDark.src = "/wp-content/plugins/te-custom-mods/images/DropdownYellow.png";
+	rightArrowImageDark.classList.add("dark");
+	rightArrow.appendChild(rightArrowImageLight);
+	rightArrow.appendChild(rightArrowImageDark);
+	galleryFooter.appendChild(rightArrow);
+	
+	for(var i = 0; i < galleryImages.length; i++) {
+		galleryInnerWrapper.appendChild(galleryImages[i]);
+		indicatorContainer.appendChild(positionIndicators[i]);
+	}
+
+	for(var i = 0; i < galleryFauxImages.length; i++) {
+		galleryInnerWrapper.appendChild(galleryFauxImages[i]);
+	}
+	gallery.appendChild(galleryInnerWrapper);
+	gallery.appendChild(indicatorContainer);
+	document.querySelector(`.tec-gallery:nth-of-type(${g_num + 1})`).appendChild(gallery);
+	document.querySelector(`.tec-gallery:nth-of-type(${g_num + 1})`).appendChild(galleryFooter);
+	return;
 }
