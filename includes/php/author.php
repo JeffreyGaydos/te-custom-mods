539a486793f99c630b92eb0e1eb90e99806e3597
JeffreyGaydos <55904615+JeffreyGaydos@users.noreply.github.com>
2024-05-05 13:24:48 -0400

    Fix PHP Errors Related to Category & Author Archive Partial Crashes (#90)
    The Author and Category pages began partially crashing (the first post appears but no others do and styling fails to load) upon the most recent updates to them despite not crashing locally. We will deploy updates to the plugin from this branch, which might cause us to increment multiple minor versions in the process.
    
    Attempts:
    1. ❌ Remove the `<<<HTML` from the category archives
    2. ✅ Remove a stray `;` that _shouldn't_ do anything but might
       - But now it crashes at the pagination code...
    
    Attempts 2:
    1. ❌ Remove the `__` because idk what that is anyways
    2. ✅ Remove a stray `;` that _shouldn't_ do anything but might
        - Oh they were down by the last divider also...

--- includes/php/author.php
+++ includes/php/author.php
@@ -60,7 +60,7 @@
 		while ( $posts->have_posts() ) : $posts->the_post();
 			$i++;
 			if ( $i > 1 ) {
-				?> <hr class="post-separator styled-separator is-style-wide section-inner" aria-hidden="true" /> <?php;
+				?> <hr class="post-separator styled-separator is-style-wide section-inner" aria-hidden="true" /> <?php
 			}
 			?>
 			<h2 class="archive-text" style="margin-left: 20%; margin-right: 20%;">
@@ -80,7 +80,7 @@
 			<?php
 		endwhile;
 		if ( get_next_posts_link() || get_previous_posts_link() ) :
-			?> <hr class="post-separator styled-separator is-style-wide section-inner" aria-hidden="true" /> <?php;
+			?> <hr class="post-separator styled-separator is-style-wide section-inner" aria-hidden="true" /> <?php
 		endif;
 		?> <p style="text-align: center; font-size: 20px">
 			<?php
