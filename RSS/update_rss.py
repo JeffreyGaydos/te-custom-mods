ca3640eb028db542bdc502a39cc1f3d670a34056
JeffreyGaydos <55904615+JeffreyGaydos@users.noreply.github.com>
2024-06-05 23:06:31 -0400

    End early so we can get our log files updated

--- RSS/update_rss.py
+++ RSS/update_rss.py
@@ -14,40 +14,40 @@ requestsResponse = requests.get("https://tanks-encyclopedia.com/", headers={'use
 rawContent = requestsResponse.content
 recentPostsRGX = "(?<=<nav aria-label=\"Recent Posts\">).+(?=</nav>)"
 print(rawContent)
-recentPostsHTML = re.search(recentPostsRGX, (str)(rawContent)).group(0)
-mostRecentPost = recentPostsHTML.split("</a>")[0]
-mostRecentPostTitle = mostRecentPost.split('">')[1]
-mostRecentPostLink = mostRecentPost.split('">')[0].split('<a href="')[1]
-mostRecentPostTitle = mostRecentPostTitle.encode('latin1').decode('unicode-escape').encode('latin1').decode('utf-8')
+#recentPostsHTML = re.search(recentPostsRGX, (str)(rawContent)).group(0)
+#mostRecentPost = recentPostsHTML.split("</a>")[0]
+#mostRecentPostTitle = mostRecentPost.split('">')[1]
+#mostRecentPostLink = mostRecentPost.split('">')[0].split('<a href="')[1]
+#mostRecentPostTitle = mostRecentPostTitle.encode('latin1').decode('unicode-escape').encode('latin1').decode('utf-8')
 
-print("Article Title: "  + mostRecentPostTitle)
-print("Article Link: " + mostRecentPostLink)
+#print("Article Title: "  + mostRecentPostTitle)
+#print("Article Link: " + mostRecentPostLink)
 
-guidRGX = '(?<=<guid>).+(?=</guid>)'
-rssR = open("./RSS/recent-articles.rss", "r")
-if(re.search(guidRGX, rssR.read()).group(0) == mostRecentPostLink):
-    print("No new article found. Skipping...")
-else:
-    dateString = (str)(datetime.datetime.now())
-    print("RSS Update Date: " + dateString[:10])
+#guidRGX = '(?<=<guid>).+(?=</guid>)'
+#rssR = open("./RSS/recent-articles.rss", "r")
+#if(re.search(guidRGX, rssR.read()).group(0) == mostRecentPostLink):
+    #print("No new article found. Skipping...")
+#else:
+    #dateString = (str)(datetime.datetime.now())
+    #print("RSS Update Date: " + dateString[:10])
 
-    #update RSS file
-    urlString = mostRecentPostLink
-    print(urlString)
-    titleString = mostRecentPostTitle
-    print(titleString)
+    ##update RSS file
+    #urlString = mostRecentPostLink
+    #print(urlString)
+    #titleString = mostRecentPostTitle
+    #print(titleString)
 
-    base = open("./RSS/recent-articles.base.rss").read()
+    #base = open("./RSS/recent-articles.base.rss").read()
 
-    # base = base.replace(_date, dateString[:10])
-    base = base.replace(_date, dateString)
-    base = base.replace(_url, urlString)
-    base = base.replace(_title, titleString)
+    ## base = base.replace(_date, dateString[:10])
+    #base = base.replace(_date, dateString)
+    #base = base.replace(_url, urlString)
+    #base = base.replace(_title, titleString)
 
-    print(base)
+    #print(base)
 
-    rss = open("./RSS/recent-articles.rss", "w", -1, "utf-8")
-    rss.write(base)
-    rss.close()
+    #rss = open("./RSS/recent-articles.rss", "w", -1, "utf-8")
+    #rss.write(base)
+    #rss.close()
 
-print("Finished updating RSS feeds...")
+#print("Finished updating RSS feeds...")
