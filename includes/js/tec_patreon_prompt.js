4b516f791f4a1531bdbcb80c8a89bf95f9b2a256
JeffreyGaydos <55904615+JeffreyGaydos@users.noreply.github.com>
2025-12-29 11:09:39 -0500

    T.E. Magazine Issue 11 Native Advertisement (#126)
    The new magazine is ready to go, so must the ad be. Also changed the behavior slightly so that returning users (visitors that have the old visited cookie present) get the new ad sent out immediately, but brand-new visitors (ones that don't have the old visited cookie) don't get the ad immediately. The ad is still clearable for 1 week at a time.
    
    - [x] Double check with stakeholders that the wording and look is acceptable
    
    <img width="295" height="320" alt="image" src="https://github.com/user-attachments/assets/2b170943-9332-4ccf-ba15-ad77733b0ccb" />

--- includes/js/tec_patreon_prompt.js
+++ includes/js/tec_patreon_prompt.js
@@ -1,6 +1,7 @@
 var te_ppp_days = 7;
-var te_visited_cookie_name = "te_visited_marker_ppp002";
-var te_closed_cookie_name = "te_patreon_closed002";
+var te_previous_visited_cookie_name = "te_visited_marker_ppp002";
+var te_visited_cookie_name = "te_visited_marker_ppp003";
+var te_closed_cookie_name = "te_patreon_closed003";
 
 function tec_closePatreonLong() {
     document.querySelector(".tec-patreon-long-box").style.display = "none";
@@ -10,14 +11,14 @@ function tec_closePatreonLong() {
 
 if(!tec_is_client_mobile) {
     var tec_patreon_long_box = document.createElement("a");
-    tec_patreon_long_box.href = "https://payhip.com/b/5n2cV";
+    tec_patreon_long_box.href = "https://payhip.com/b/GWRPn";
     tec_patreon_long_box.className = "tec-patreon-long-box";
     var tec_patreon_long_te_logo = document.createElement("img");
-    tec_patreon_long_te_logo.src = "/wp-content/plugins/te-custom-mods/images/All-Pack-New_800x600.jpg";
-    tec_patreon_long_te_logo.className = "tec-patreon-long-te-logo";
+    tec_patreon_long_te_logo.src = "/wp-content/plugins/te-custom-mods/images/TE_Mag_011.png";
+    tec_patreon_long_te_logo.className = "tec-patreon-long-te-logo portrait";
     var tec_patreon_long_text = document.createElement("p");
-    tec_patreon_long_text.innerText = "Click to buy!";
-    tec_patreon_long_text.className = "tec-patreon-long-text";
+    tec_patreon_long_text.innerText = "TE Magazine Issue #11 is Out Now! Click to Buy!";
+    tec_patreon_long_text.className = "tec-patreon-long-text portrait";
     // var tec_patreon_long_patreon_logo = document.createElement("img");
     // tec_patreon_long_patreon_logo.src = "/wp-content/plugins/te-custom-mods/images/PatreonLogo.png";
     // tec_patreon_long_patreon_logo.className = "tec-patreon-long-patreon-logo";
@@ -26,14 +27,19 @@ if(!tec_is_client_mobile) {
     tec_patreon_long_x.innerHTML = "&Cross;";
     tec_patreon_long_x.addEventListener("click", () => tec_closePatreonLong());
     
-    tec_patreon_long_box.appendChild(tec_patreon_long_text);
     tec_patreon_long_box.appendChild(tec_patreon_long_te_logo);
+    tec_patreon_long_box.appendChild(tec_patreon_long_text);
     // tec_patreon_long_box.appendChild(tec_patreon_long_patreon_logo);
     
     var visited_cookie = document.cookie.split("; ").filter(c => c.startsWith(te_visited_cookie_name))[0];
+    var previous_visited_cookie = document.cookie.split("; ").filter(c => c.startsWith(te_previous_visited_cookie_name))[0];
     if(!document.cookie.split("; ").filter(c => c.startsWith(te_closed_cookie_name))[0]
-        && visited_cookie
-        && ((Date.now() + 1000*60*60*24*365) - visited_cookie.split('=')[1]) > (1000*60*60*24*te_ppp_days)) {
+        && (visited_cookie || previous_visited_cookie)
+        && (
+            ((Date.now() + 1000*60*60*24*365) - visited_cookie.split('=')[1]) > (1000*60*60*24*te_ppp_days)
+            ||
+            ((Date.now() + 1000*60*60*24*365) - previous_visited_cookie.split('=')[1]) > (1000*60*60*24*te_ppp_days)
+        )) {
         document.querySelector("body").appendChild(tec_patreon_long_box);
         document.querySelector("body").appendChild(tec_patreon_long_x);
     
