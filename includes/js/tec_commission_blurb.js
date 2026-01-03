cc50eaabc767602401f95292a935be980c469a88
JeffreyGaydos <jeff13gaydos@gmail.com>
2026-01-03 00:35:50 -0500

    integrate commission blurb (bypass original "end of section" finding behavior)

--- includes/js/tec_commission_blurb.js
+++ includes/js/tec_commission_blurb.js
@@ -2,8 +2,8 @@ function tec_placeCommissionBlurb() {
     if(document.querySelector('a[href*="https://amzn.to"], a[href*="&tag=tankencyclope-20"]')) {
         if(document.querySelector('span.button a[href*="https://amzn.to"], span.button a[href*="&tag=tankencyclope-20"]')) {
             //a <span> with a button class is always the old styling from david. There are usually not any links in the sources section, so let's put it at the bottom of the article since that's where the massive amazon book ads are
-            document.querySelector(".entry-content").classList.add("tec-commission-blurb");
-            document.querySelector(".entry-content").classList.add("tec-commission-skinny");
+            document.querySelector(".entry-content, .main-raised").classList.add("tec-commission-blurb");
+            document.querySelector(".entry-content, .main-raised").classList.add("tec-commission-skinny");
         } else {
             const sourceHeader = tec_find_source_header();
             tec_tag_source_section(sourceHeader);
@@ -12,18 +12,18 @@ function tec_placeCommissionBlurb() {
 }
 
 function tec_find_source_header() {
-    const validHeaders = ["Sources", "source"];
+    const validHeaders = ["Sources", "source", "Sources:"];
     const validHeaderContains = ["Links and references", "Links and reference", "Link and references", "Link and reference"];
     var result = [];
-    document.querySelectorAll(".entry-content h3:not(#tec_indexBox), .entry-content h2:not(#tec_indexBox)").forEach(n => {
+    document.querySelectorAll(".entry-content h3:not(#tec_indexBox), .entry-content h2:not(#tec_indexBox), .main-raised h3:not(#toc), .main-raised h2:not(#toc)").forEach(n => {
         for(var i = 0; i < validHeaders.length; i++) {
-            if(n.textContent.toUpperCase() == validHeaders[i].toUpperCase()) {
+            if(n.textContent.trim().toUpperCase() == validHeaders[i].toUpperCase()) {
                 result.push(n);
                 return;
             }
         }
         for(var i = 0; i < validHeaderContains.length; i++) {
-            if(n.textContent.toUpperCase().includes(validHeaderContains[i].toUpperCase())) {
+            if(n.textContent.trim().toUpperCase().includes(validHeaderContains[i].toUpperCase())) {
                 result.push(n);
                 return;
             }
@@ -35,6 +35,13 @@ function tec_find_source_header() {
 function tec_tag_source_section(sourceNode) {
     let addedBlurb = false;
     if(sourceNode) {
+        //New theme behavior
+        if(sourceNode.parentElement.tagName == 'SECTION') {
+            sourceNode.parentElement.classList.add("tec-commission-blurb");
+            addedBlurb = true;
+            return;
+        }
+        //Original plugin behavior
         const nextElement = sourceNode.nextElementSibling;
         switch(nextElement.tagName) {
             case 'P':
@@ -52,8 +59,8 @@ function tec_tag_source_section(sourceNode) {
         }
     }
     if(!addedBlurb) {
-        document.querySelector(".entry-content").classList.add("tec-commission-blurb");
-        document.querySelector(".entry-content").classList.add("tec-commission-skinny");
+        document.querySelector(".entry-content, .main-raised").classList.add("tec-commission-blurb");
+        document.querySelector(".entry-content, .main-raised").classList.add("tec-commission-skinny");
     }
 }
 
