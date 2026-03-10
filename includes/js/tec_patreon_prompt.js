565e5e4acd3b22d30cb6a9d30c71eae64afa2b6b
JeffreyGaydos <jeff13gaydos@gmail.com>
2026-03-09 21:13:16 -0400

    Update ad text and link

--- includes/js/tec_patreon_prompt.js
+++ includes/js/tec_patreon_prompt.js
@@ -1,7 +1,7 @@
 var te_ppp_days = 7;
-var te_previous_visited_cookie_name = "te_visited_marker_ppp003";
-var te_visited_cookie_name = "te_visited_marker_ppp004";
-var te_closed_cookie_name = "te_patreon_closed004";
+var te_previous_visited_cookie_name = "te_visited_marker_ppp004";
+var te_visited_cookie_name = "te_visited_marker_ppp005";
+var te_closed_cookie_name = "te_patreon_closed005";
 
 function tec_closePatreonLong() {
     document.querySelector(".tec-patreon-long-box").style.display = "none";
@@ -11,13 +11,14 @@ function tec_closePatreonLong() {
 
 if(!tec_is_client_mobile) {
     var tec_patreon_long_box = document.createElement("a");
-    tec_patreon_long_box.href = "https://tanks-encyclopedia.com/tank-encyclopedia-magazine-all-pack-11-issues-special-offer";
+    tec_patreon_long_box.href = "https://tanks-encyclopedia.com/extension-tank-encyclopedia-magazine-all-pack-11-issues-special-offer/";
     tec_patreon_long_box.className = "tec-patreon-long-box";
     var tec_patreon_long_te_logo = document.createElement("img");
     tec_patreon_long_te_logo.src = "/wp-content/plugins/te-custom-mods/images/TE_FebruarySale_2026.png";
     tec_patreon_long_te_logo.className = "tec-patreon-long-te-logo portrait";
+    tec_patreon_long_te_logo.alt = "11 stacked Magazines; Tank Encyclopedia All Pack 25% February Sale";
     var tec_patreon_long_text = document.createElement("p");
-    tec_patreon_long_text.innerText = "All-pack magazine sale ends March 8th! Click to Buy!";
+    tec_patreon_long_text.innerText = "Sale extended: now ends March 31st! Click to buy!";
     tec_patreon_long_text.className = "tec-patreon-long-text portrait";
     // var tec_patreon_long_patreon_logo = document.createElement("img");
     // tec_patreon_long_patreon_logo.src = "/wp-content/plugins/te-custom-mods/images/PatreonLogo.png";
