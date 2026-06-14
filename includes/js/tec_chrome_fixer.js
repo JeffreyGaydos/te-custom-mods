3aa3ed92c13dfcad94dfb1b58e8b9677bd4d58a2
JeffreyGaydos <55904615+JeffreyGaydos@users.noreply.github.com>
2026-06-13 21:29:06 -0400

    Gemini Link Removal: Full Removals and More Timeout (#143)
    We don't control when these links appear, so it's hard to test this
    without sending it. This should help to remove more/all links
    
    ---------
    
    Co-authored-by: GitHub Actions <actions@github.com>

--- includes/js/tec_chrome_fixer.js
+++ includes/js/tec_chrome_fixer.js
@@ -1,32 +1,29 @@
-var tec_killCounter = 0;
+if (navigator.userAgent.indexOf("Chrome") != -1 && navigator.userAgent.indexOf("Edg") == -1) {
+    var tec_killCounter = 0;
 
-var tec_chrome_fixer = setInterval(() => {
-    var tec_googleElements = [];
-    var tec_googleLinks = [];
-    var tec_anyWithExplicitStyle = document.querySelectorAll("*[style]").forEach(s => {
-        if(s.getAttribute("style").length > 5000) {
-            if(s.tagName.toLowerCase() == "a") {
-                tec_googleLinks.push(s);
+    var tec_chrome_fixer = setInterval(() => {
+        var tec_googleElements = [];
+        var tec_googleLinks = [];
+        var tec_anyWithExplicitStyle = document.querySelectorAll("*[style]").forEach(s => {
+            if(s.getAttribute("style").length > 5000) {
+                if(s.tagName.toLowerCase() == "a") {
+                    tec_googleLinks.push(s);
+                }
+                else {
+                    tec_googleElements.push(s);
+                }
             }
-            else {
-                tec_googleElements.push(s);
-            }
-        }
-    });
-    tec_killCounter += tec_googleElements.length;
-    tec_killCounter += tec_googleLinks.length;
-    tec_googleElements.forEach(e => e.style = "");
-    tec_googleLinks.forEach(e => {
-        var text = e.innerText;
-        var parent = e.parentElement;
-        var normal = document.createElement("SPAN");
-        normal.innerText = text;    
-        parent.insertBefore(normal, e);
-        e.remove();
-    });
-}, 1000);
-
-setTimeout(() => {
-    clearInterval(tec_chrome_fixer);
-    console.log(`tec_chrome_fixer: ${tec_killCounter}`);
-}, 20000);
+        });
+        tec_killCounter += tec_googleElements.length;
+        tec_killCounter += tec_googleLinks.length;
+        tec_googleLinks.forEach(e => {
+            var text = e.innerText;
+            var parent = e.parentElement;
+            var normal = document.createElement("SPAN");
+            normal.innerText = text;    
+            parent.insertBefore(normal, e);
+            e.remove();
+        });
+        tec_googleElements.forEach(e => e.remove());
+    }, 1000);
+}
